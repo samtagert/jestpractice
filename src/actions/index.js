58a169f26d14a5ca169daa903964df64651e6d43
Sam Tagert <samtagert@gmail.com>
2019-08-11 23:49:50 -0700

    refactor index.js to have redux setup in Root.js for test files

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+import { SAVE_COMMENT } from 'actions/types'
+export function saveComment(comment) {
+  return {
+    type: SAVE_COMMENT,
+    payload: comment
+  };
+}
--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import * as actions from 'actions';
 
 class CommentBox extends React.Component {
   state = { comment: '' };
@@ -9,7 +11,7 @@ class CommentBox extends React.Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    // Action creator
+    this.props.saveComment(this.state.comment);
     this.setState({ comment: '' })
   }
 
@@ -26,4 +28,4 @@ class CommentBox extends React.Component {
   };
 };
 
-export default CommentBox;
+export default connect(null, actions)(CommentBox);
