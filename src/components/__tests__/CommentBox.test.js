0ec6a5fe227cee6890c62e1c9fe8188ac3a22593
Sam Tagert <samtagert@gmail.com>
2019-08-11 23:26:51 -0700

    inital redux setup

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -17,23 +17,24 @@ it('has a text area and a button', () => {
   expect(wrapped.find('button').length).toEqual(1);
 });
 
-it('has a text area that users can type in', () => {
-  // actual html event, not the react name
-  wrapped.find('textarea').simulate('change', {
-    // merges into the event argument (so it'll go "event" .target.value (from below))
-    target: { value: 'new comment' }
-  }); // can't check for update right away because react rendering after setstate is async, need to force update
-  wrapped.update();
-  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
-});
-
-it('has a form that clears its textarea after submission', () => {
-  wrapped.find('textarea').simulate('change', {
-    target: { value: 'new comment' }
+describe('the textarea', () => {
+  beforeEach(() => {
+    // actual html event, not the react name
+    wrapped.find('textarea').simulate('change', {
+      // merges into the event argument (so it'll go "event" .target.value (from below))
+      target: { value: 'new comment' }
+    }); // can't check for update right away because react rendering after setstate is async, need to force update
+    wrapped.update();
   });
-  wrapped.update();
-  // should consider separate expect statement here to check if comment actually updated, but ok to skip because above test does that for us
-  wrapped.find('form').simulate('submit');
-  wrapped.update();
-  expect(wrapped.find('textarea').prop('value')).toEqual('');
-});
+
+  it('has a text area that users can type in', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  });
+
+  it('has a form that clears its textarea after submission', () => {
+    // should consider separate expect statement here to check if comment actually updated, but ok to skip because above test does that for us
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
+})
