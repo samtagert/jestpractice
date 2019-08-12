005ea384079aadc28858bd1f4ec16e3e09979d8b
Sam Tagert <samtagert@gmail.com>
2019-08-12 01:41:47 -0700

    integration test for fetching comments with axios/moxios

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -1,13 +1,35 @@
 // in a larger application, could make more sense to separate integration test files and name commentsIntegrations etc
 import React from 'react';
 import { mount } from 'enzyme';
+import moxios from 'moxios';
 import Root from 'Root';
 import App from 'components/App';
 
-it('can fetch a list of comments and display them', () => {
-  // attempt to render the *entire* app
+beforeEach(() => {
+  moxios.install(); // stops all axios calls from running
+  moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+    status: 200,
+    response: [{ name: 'comment 1' }, { name: 'comment 2' }]
+  }); // intercept call, respond with data, and trick axios into thinking it got a response
+});
 
-  // find the 'fetchComments' button and click it
+afterEach(() => {
+  moxios.uninstall();
+});
 
-  // expect to find a list of comments
-})
+it('can fetch a list of comments and display them', done => {
+  const wrapped = mount(
+    <Root>
+      <App />
+    </Root>
+  );
+
+  wrapped.find('.fetch-comments').simulate('click');
+
+  moxios.wait(() => {
+    wrapped.update();
+    expect(wrapped.find('li').length).toEqual(2);
+    done();
+    wrapped.unmount();
+  });
+});
