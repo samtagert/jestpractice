3b5411c14d98d5e1867941b9842c1190ed7b88c0
Sam Tagert <samtagert@gmail.com>
2019-08-12 00:52:42 -0700

    able to make network requests to json placeholder for comments

---
+++ src/__tests__/integrations.test.js
@@ -0,0 +1,13 @@
+// in a larger application, could make more sense to separate integration test files and name commentsIntegrations etc
+import React from 'react';
+import { mount } from 'enzyme';
+import Root from 'Root';
+import App from 'components/App';
+
+it('can fetch a list of comments and display them', () => {
+  // attempt to render the *entire* app
+
+  // find the 'fetchComments' button and click it
+
+  // expect to find a list of comments
+})
