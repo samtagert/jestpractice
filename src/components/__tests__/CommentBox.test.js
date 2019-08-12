2752de408999b292b26de90acb8495f08d232700
Sam Tagert <samtagert@gmail.com>
2019-08-11 23:09:52 -0700

    add form test

---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import CommentBox from 'components/CommentBox';
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
+it('has a text area and a button', () => {
+  expect(wrapped.find('textarea').length).toEqual(1);
+  expect(wrapped.find('button').length).toEqual(1);
+});
+
+it('has a text area that users can type in', () => {
+  // actual html event, not the react name
+  wrapped.find('textarea').simulate('change', {
+    // merges into the event argument (so it'll go "event" .target.value (from below))
+    target: { value: 'new comment' }
+  }); // can't check for update right away because react rendering after setstate is async, need to force update
+  wrapped.update();
+  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+});
+
+it('has a form that clears its textarea after submission', () => {
+  wrapped.find('textarea').simulate('change', {
+    target: { value: 'new comment' }
+  });
+  wrapped.update();
+  // should consider separate expect statement here to check if comment actually updated, but ok to skip because above test does that for us
+  wrapped.find('form').simulate('submit');
+  wrapped.update();
+  expect(wrapped.find('textarea').prop('value')).toEqual('');
+});
